7aad6802dcf6ba02eeb5096e07a767b2dd35d07b
Denys <denisbadakaty51@gmail.com>
2023-07-13 22:03:42 +0300

    fixed some bugs with rendering spinner

--- client/src/view/calculatorView/events/customEvent/CustomEvent.ts
+++ client/src/view/calculatorView/events/customEvent/CustomEvent.ts
@@ -25,6 +25,7 @@ class CustomEvents {
 
   private calculateExpressionEvent() {
     const input: HTMLInputElement = document.querySelector(`.${inputClassNames.inputField}`);
+    const outputField = document.querySelector(`.${outputClassNames.resultField}`);
 
     if (this.model.getExpression() === input.value) {
       if (this.model.getErrorMessage()) {
@@ -33,19 +34,20 @@ class CustomEvents {
       }
       return;
     }
+
     this.model.clearErrorField();
-    if (input.value) {
-      setTimeout(() => {
-        const outputField = document.querySelector(`.${outputClassNames.resultField}`);
-        if (outputField.textContent) return;
-        document.querySelector(`.${outputClassNames.resultField}`).textContent = '';
-        Spinner.render(`${outputClassNames.resultField}`);
-        const spinner: HTMLElement = document.querySelector(`.${spinnerClassNames.root}`);
-        spinner.style.height = '20px';
-        spinner.style.width = '20px';
-        spinner.style.borderWidth = '5px';
-      }, 200);
-    }
+    if (!input.value) return;
+    outputField.textContent = '';
+    setTimeout(() => {
+      if (outputField.textContent) return;
+      document.querySelector(`.${outputClassNames.resultField}`).textContent = '';
+      Spinner.render(`${outputClassNames.resultField}`);
+      const spinner: HTMLElement = document.querySelector(`.${spinnerClassNames.root}`);
+      spinner.style.height = '20px';
+      spinner.style.width = '20px';
+      spinner.style.borderWidth = '5px';
+    }, 200);
+
     this.model.setExpression(input.value);
   }
 }

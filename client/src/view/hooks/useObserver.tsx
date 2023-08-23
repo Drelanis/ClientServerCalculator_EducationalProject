e608966734d009ce476b80693ed759ff9e5116d5
Denys <denisbadakaty51@gmail.com>
2023-08-23 10:31:35 +0300

    some fix

--- client/src/view/hooks/useObserver.tsx
+++ client/src/view/hooks/useObserver.tsx
@@ -1,49 +1,31 @@
-// @ts-nocheck
 import { useEffect, useRef, RefObject } from 'react';
 
-// const useObserver = (
-//   ref: RefObject<HTMLElement | null>,
-//   canLoad: boolean,
-//   isLoading: boolean,
-//   callback: () => void
-// ) => {
-//   const observer = useRef<IntersectionObserver | null>(null);
-//   useEffect(() => {
-//     if (isLoading) return;
-//     if (observer.current) {
-//       observer.current.disconnect();
-//     }
-//     const cb: IntersectionObserverCallback = (entries, observer) => {
-//       if (entries[0].isIntersecting && canLoad) {
-//         callback();
-//       }
-//     };
-//     observer.current = new IntersectionObserver(cb);
-//     if (ref.current) {
-//       observer.current.observe(ref.current);
-//     }
-//     return () => {
-//       if (observer.current) {
-//         observer.current.disconnect();
-//       }
-//     };
-//   }, [isLoading]);
-// };
-
-// export default useObserver;
-
-const useObserver = (ref, canLoad, isLoading, callback) => {
-  const observer = useRef();
+const useObserver = (
+  ref: RefObject<HTMLElement | null>,
+  canLoad: boolean,
+  isLoading: boolean,
+  callback: () => void
+) => {
+  const observer = useRef<IntersectionObserver | null>(null);
   useEffect(() => {
     if (isLoading) return;
-    if (observer.current) observer.current.disconnect();
-    const cb = (entries, observer) => {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+    const cb: IntersectionObserverCallback = (entries, observer) => {
       if (entries[0].isIntersecting && canLoad) {
         callback();
       }
     };
     observer.current = new IntersectionObserver(cb);
-    observer.current.observe(ref.current);
+    if (ref.current) {
+      observer.current.observe(ref.current);
+    }
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
   }, [isLoading]);
 };
 
